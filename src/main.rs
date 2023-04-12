9481433d99a1be94a4ab6c6457a1077709ac2875
Connectety-T <k8ieunomu@mozmail.com>
2023-04-12 23:00:56 +0200

    add prototype validation for cbor and json.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ extern crate rocket;
 
 extern crate rocket_dyn_templates;
 
+use std::io::Read;
 use rocket::Request;
 use rocket::{Build, Rocket};
 use rocket::form::Form;
 use rocket_dyn_templates::{context, Template};
-use rocket::fs::{FileServer, relative};
+use rocket::fs::{FileServer, relative, TempFile};
 use crate::validation::{ValidationLibrary, ValidationType};
 
 #[get("/")]
@@ -17,18 +18,52 @@ fn index() -> Template {
     Template::render("index", context! {})
 }
 
+#[non_exhaustive]
+#[derive(FromFormField, Clone)]
+enum PlainValidationType {
+    #[field(value = "")]
+    Plain,
+    #[field(value = "json")]
+    WithJson,
+    #[field(value = "cbor")]
+    WithCbor,
+}
+
 #[derive(FromForm)]
 struct Validation<'r> {
     cddl: &'r str,
     crateT: ValidationLibrary,
+    vtype: PlainValidationType,
+    extra: &'r str,
+    file: TempFile<'r>,
+}
+
+fn get_temp_file_content(file: &TempFile) -> Result<Vec<u8>, std::io::Error> {
+    match file {
+            TempFile::Buffered { content } => Ok(content.as_bytes().to_vec()),
+            TempFile::File { path, len, .. } => {
+                let mut file = std::fs::File::open(path.as_ref().left().unwrap())?;
+                let mut bytes = Vec::with_capacity(*len as usize);
+                file.read_to_end(&mut bytes)?;
+                Ok(bytes)
+            },
+        }
 }
 
 #[post("/validate", data = "<validation_data>")]
 fn validate(validation_data: Form<Validation<'_>>) -> Template {
-    let result = validation::validate(
-        validation_data.crateT.clone(),
-        ValidationType::Plain(validation_data.cddl.to_string()),
-    );
+    let form_cddl = validation_data.cddl.to_string();
+    let validation_type = match validation_data.vtype {
+        PlainValidationType::Plain => ValidationType::Plain(form_cddl),
+        PlainValidationType::WithJson => {
+            ValidationType::WithJson(form_cddl, validation_data.extra.to_string())
+        }
+        PlainValidationType::WithCbor => {
+            ValidationType::WithCbor(form_cddl, get_temp_file_content(&validation_data.file).unwrap())
+        }
+    };
+
+    let result = validation::validate(validation_data.crateT.clone(), validation_type);
 
     if result.is_ok() {
         return Template::render("response", context! {
