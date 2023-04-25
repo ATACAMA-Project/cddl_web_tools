182e2a7968c925c87aacf64dcc0ffc8e7914ff5b
Connectety-T <k8ieunomu@mozmail.com>
2023-04-25 22:51:28 +0200

    remove cors.

--- src/main.rs
+++ src/main.rs
@@ -93,24 +93,4 @@ fn rocket() -> Rocket<Build> {
         .mount("/", routes![index, validate])
         .mount("/static", FileServer::from(relative!("static")))
         .attach(Template::fairing())
-        .attach(CORS)
-}
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
 }
